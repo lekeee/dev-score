d7663c7571a99c73d788b05b4f480adbfd465910
lekeee <jaleksa388@gmail.com>
2024-09-28 13:14:00 +0200

    user guard redirect

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,11 +2,9 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { map, take } from 'rxjs';
-import { AuthService } from '../services/auth/auth.service';
 import { selectIsLoggedIn } from '../store/auth/auth.selectors';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  const auth = inject(AuthService);
   const router = inject(Router);
   const store = inject(Store);
 
--- src/app/core/guards/user.guard.ts
+++ src/app/core/guards/user.guard.ts
@@ -1,18 +1,20 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { map, take } from 'rxjs';
-import { AuthService } from '../services/auth/auth.service';
 import { selectIsLoggedIn } from '../store/auth/auth.selectors';
 
 export const userGuard: CanActivateFn = (route, state) => {
-  const auth = inject(AuthService);
+  const router = inject(Router);
   const store = inject(Store);
 
   return store.select(selectIsLoggedIn).pipe(
     take(1),
     map((isLoggedIn) => {
-      return isLoggedIn ? false : true;
+      if (isLoggedIn) {
+        router.navigate(['']);
+        return false;
+      } else return true;
     })
   );
 };
